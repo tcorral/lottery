9cb3c393498d23be9f52bf358aa355bf6a8789f9
tcorral <amischol@gmail.com>
2014-10-05 19:32:01 +0200

    fix some warnings

--- lottery.js
+++ lottery.js
@@ -1,4 +1,13 @@
-var calculateResult = function () {
+var calculateResult = function (whiteBalls, powerBall) {
+  var index;
+  for(index = 0; index < 5; index++){
+    if((whiteBalls[index] < 1) || (whiteBalls[index] > 59)) {
+      return -1;
+    }
+  }
+  if((powerBall < 1) || (powerBall > 39)) {
+    return -1;
+  }
   return 0;
 };
 var lottery = function () {
